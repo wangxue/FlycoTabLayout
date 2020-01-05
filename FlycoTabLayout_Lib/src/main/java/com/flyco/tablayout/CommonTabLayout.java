6d5fd87e54e43e8c4d1d7d92b75a5fa604268843
wangxue02 <wangxue02@ky-tech.com.cn>
2020-01-05 17:45:17 +0800

    CommonTabLayout切换状态时仅修改select状态，不直接设置drawable

--- FlycoTabLayout_Lib/src/main/java/com/flyco/tablayout/CommonTabLayout.java
+++ FlycoTabLayout_Lib/src/main/java/com/flyco/tablayout/CommonTabLayout.java
@@ -312,10 +312,9 @@ public class CommonTabLayout extends FrameLayout implements ValueAnimator.Animat
             View tabView = mTabsContainer.getChildAt(i);
             final boolean isSelect = i == position;
             TextView tab_title = (TextView) tabView.findViewById(R.id.tv_tab_title);
-            tab_title.setTextColor(isSelect ? mTextSelectColor : mTextUnselectColor);
             ImageView iv_tab_icon = (ImageView) tabView.findViewById(R.id.iv_tab_icon);
-            CustomTabEntity tabEntity = mTabEntitys.get(i);
-            iv_tab_icon.setImageResource(isSelect ? tabEntity.getTabSelectedIcon() : tabEntity.getTabUnselectedIcon());
+            tab_title.setSelected(isSelect);
+            iv_tab_icon.setSelected(isSelect);
             if (mTextBold == TEXT_BOLD_WHEN_SELECT) {
                 tab_title.getPaint().setFakeBoldText(isSelect);
             }
